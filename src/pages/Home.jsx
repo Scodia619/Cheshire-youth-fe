8eb86e861ea219b6ddca79d30b1d39ae31b1acd7
Amy Mules <amymules@Amys-MacBook-Air.local>
2023-12-21 23:31:20 +0000

    fetched data and added to page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,26 @@
 import { useEffect, useState } from "react"
 import axios from 'axios'
+import { Link } from "react-router-dom"
 
 const Home = () => {
 
     const [commissions, setCommissions] = useState([])
 
     useEffect(()=>{
-        axios.get
+        axios.get('https://cheshire-youth-server.onrender.com/api/commission').then(({data})=>{
+            setCommissions(data.commissions)
+        })
     }, [])
 
-    return <h1>Home Screen</h1>
+    return (
+        <>
+        {commissions.map(commission => {
+            return <Link to={commission.commission} key={commission.commission_id}>
+                <h1>{commission.commission}</h1>
+            </Link>
+        })}
+        </>
+    )
 }
 
 export default Home
