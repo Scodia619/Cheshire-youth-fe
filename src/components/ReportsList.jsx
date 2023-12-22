0ce110ae5fcb05a2ff7efca8bb79aa45ba41814b
Amy Mules <amymules@Amys-MacBook-Air.local>
2023-12-22 00:20:16 +0000

    Adding links to go to commission page

--- src/components/ReportsList.jsx
+++ src/components/ReportsList.jsx
@@ -1,18 +1,43 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect} from "react";
+import { useParams } from "react-router-dom";
 import axios from "axios";
 
 const ReportsList = () => {
+
+  const {commission} = useParams();
+
   const [reports, setReports] = useState([]);
+  const [noData, setNoData] = useState(true)
+  const [isLoading, setIsLoading] = useState(true)
+  const [isError, setIsError] = useState(false)
 
   useEffect(() => {
     axios
-      .get("https://cheshire-youth-server.onrender.com/api/reports/cheshire")
+      .get(`https://cheshire-youth-server.onrender.com/api/reports/${commission}`)
       .then(({ data }) => {
-        setReports(data.reports);
+        if(data.reports.length){
+          setReports(data.reports);
+          setNoData(false)
+        }
+      }).catch(()=>{
+        setIsError(true)
+      }).finally(()=>{
+        setIsLoading(false)
       });
   }, []);
 
   console.log(reports);
+
+  if(isError){
+    return <h1>Error Loading Page</h1>
+  }
+
+  if(isLoading){
+    return <h1>Loading Data</h1>
+  }
+  if(noData){
+    return <h1>No Data for commission</h1>
+  }
 
   return (
     <table>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,36 @@
-import { useEffect, useState } from "react"
-import axios from 'axios'
-import { Link } from "react-router-dom"
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 const Home = () => {
+  const [commissions, setCommissions] = useState([]);
 
-    const [commissions, setCommissions] = useState([])
+  useEffect(() => {
+    axios
+      .get("https://cheshire-youth-server.onrender.com/api/commission")
+      .then(({ data }) => {
+        setCommissions(data.commissions);
+      });
+  }, []);
 
-    useEffect(()=>{
-        axios.get('https://cheshire-youth-server.onrender.com/api/commission').then(({data})=>{
-            setCommissions(data.commissions)
-        })
-    }, [])
+  return (
+    <section className="commissions-container">
+      {commissions.map((commission) => {
+        return (
+          <Link to={commission.commission} key={commission.commission_id}>
+            <div className="commissions-selector">
+              <h1>{commission.commission}</h1>
+              <img
+              className="commission-image"
+                src={commission.commission_image}
+                alt="picture of commission"
+              />
+            </div>
+          </Link>
+        );
+      })}
+    </section>
+  );
+};
 
-    return (
-        <>
-        {commissions.map(commission => {
-            return <Link to={commission.commission} key={commission.commission_id}>
-                <h1>{commission.commission}</h1>
-            </Link>
-        })}
-        </>
-    )
-}
-
-export default Home
+export default Home;
