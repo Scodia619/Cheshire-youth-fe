7d4b6c919908fbad348cd7bd164307030064a571
Amy Mules <amymules@Amys-MacBook-Air.local>
2023-12-22 02:03:18 +0000

    adding the login feature to the webpage so you can login to an account

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,13 @@
 import { useState } from "react";
+import axios from "axios";
+
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const navigate = useNavigate();
+
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
@@ -12,9 +19,41 @@ const Login = () => {
     setPassword(e.target.value);
   };
 
+  const notify = (message) => {
+    toast.error(message, {
+      position: "top-center",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const loginData = {
+      username,
+      password,
+    };
+    axios
+      .post(
+        `https://cheshire-youth-server.onrender.com/api/users/login`,
+        loginData
+      )
+      .then(({ data }) => {
+        navigate("/");
+      })
+      .catch(({ response: { data } }) => {
+        notify(data.msg);
+      });
+  };
+
   return (
     <div className="login-container">
-      <form className="login-form">
+      <form className="login-form" onSubmit={handleSubmit}>
         <label htmlFor="username">
           Username:
           <input
@@ -35,6 +74,18 @@ const Login = () => {
         </label>
         <button>Login</button>
       </form>
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
     </div>
   );
 };
